e8d8038750fb33665b286cb0bbc6b390b0b73ec6
ealexseen <yauheni.aliakseyenko@neklo.com>
2019-10-02 11:41:07 +0300

    test

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ self.addEventListener('install', function(event) {
                 '/test-work/',
                 '/test-work/index.html',
                 '/test-work/app.js',
+                '/test-work/video/SampleVideo_720x480_30mb.mp4',
                 '/test-work/sw.js'
             ]);
         })
